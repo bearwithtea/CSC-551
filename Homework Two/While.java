2ba23f083050d8b28b421081626c205201e80446
bearwithtea <owen.c.mcgrath@icloud.com>
2025-02-04 16:02:51 -0600

    finished q3

--- Homework Two/While.java
+++ Homework Two/While.java
@@ -1,12 +1,13 @@
 /**
  * Derived class that represents a while statement in the SILLY language.
- *   @author Dave Reed
- *   @version 1/20/25
+ *  @author Dave Reed
+ *  @version 1/20/25
  */
 public class While extends Statement {
+
     private Expression expr;
-    private Compound body;  
-    
+    private Compound body;
+
     /**
      * Reads in a while statement from the specified stream
      *   @param input the stream to be read from
@@ -15,7 +16,7 @@ public class While extends Statement {
         if (!input.next().toString().equals("while")) {
             throw new Exception("SYNTAX ERROR: Malformed while statement");
         }
-        this.expr = new Expression(input);     
+        this.expr = new Expression(input);
         this.body = new Compound(input);
     }
 
@@ -23,22 +24,22 @@ public class While extends Statement {
      * Executes the current while statement.
      */
     public void execute() throws Exception {
-    	boolean keepLooping = true;
+        boolean keepLooping = true;
         while (keepLooping) {
-        	DataValue eVal = this.expr.evaluate();
-        	if (eVal.getType() != DataValue.Type.BOOLEAN) {
-        		throw new Exception("RUNTIME ERROR: while statement requires Boolean test.");
-        	}
-            if ((Boolean)eVal.getValue()) {
-            	this.body.execute();
+            DataValue eVal = this.expr.evaluate();
+            if (eVal.getType() != DataValue.Type.BOOLEAN) {
+                throw new Exception(
+                    "RUNTIME ERROR: while statement requires Boolean test."
+                );
             }
-            else {
-            	keepLooping = false;
-            }   
+            if ((Boolean) eVal.getValue()) {
+                this.body.execute();
+            } else {
+                keepLooping = false;
+            }
         }
     }
 
-    
     /**
      * Converts the current while statement into a String.
      *   @return the String representation of this statement
