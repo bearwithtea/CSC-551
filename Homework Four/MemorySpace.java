5fd0f722c6303908023f98454db1fef75d48a814
bearwithtea <owen.c.mcgrath@icloud.com>
2025-03-21 12:24:16 -0500

    refactor: Improve variable storage logic in MemorySpace and enhance test cases

--- Homework Four/MemorySpace.java
+++ Homework Four/MemorySpace.java
@@ -63,30 +63,65 @@ public class MemorySpace {
     }
 
     /**
-     * Stores a variable/value in the runtime stack.
+     * Stores a value in memory.
      *
-     * @param variable the variable name
-     * @param val      the value to be stored under that name
+     * @param variable the variable to store the value for
+     * @param val      the value to be stored
      */
     public void storeValue(Token variable, DataValue val) {
         ScopeRec currentScope = this.runtimeStack.peek();
-        ScopeRec existingScope = findScopeinStack(variable);
 
+        // if the variable is already declared in the current scope, update the value
         if (currentScope.declaredInScope(variable)) {
-            // If declared in current scope, update it there
             currentScope.storeInScope(variable, val);
-        } else if (currentScope.isFunctionScope() && existingScope != null && existingScope != currentScope) {
-            // In a function scope, if variable exists in parent scope, create a new local
-            // variable instead
-            declareVariable(variable);
-            currentScope.storeInScope(variable, val);
-        } else if (existingScope != null) {
-            // Otherwise, update the variable in its original scope
-            existingScope.storeInScope(variable, val);
         } else {
-            // Not declared anywhere, create in current scope
-            declareVariable(variable);
-            currentScope.storeInScope(variable, val);
+            // are we in a function scope?
+            ScopeRec functionScope = null;
+            ScopeRec scopeWalker = currentScope;
+
+            // walk through the scope chain to find the function scope
+            while (scopeWalker != null) {
+                if (scopeWalker.isFunctionScope()) {
+                    // found the function scope
+                    functionScope = scopeWalker;
+                    break;
+                }
+                scopeWalker = scopeWalker.getParentScope();
+            }
+
+            // look for the variable within the function's scope first
+            ScopeRec variableScope = null;
+            if (functionScope != null) {
+                // if there is not a scope, move to the parent scope until the variable is found
+                scopeWalker = currentScope;
+                while (scopeWalker != null && scopeWalker != functionScope.getParentScope()) {
+                    if (scopeWalker.declaredInScope(variable)) {
+                        variableScope = scopeWalker;
+                        break;
+                    }
+                    scopeWalker = scopeWalker.getParentScope();
+                }
+            }
+
+            // if the variable is not found in the function scope and the variable scope is
+            // null
+            if (functionScope != null && variableScope == null) {
+                // create the scope at the function level
+                functionScope.storeInScope(variable, val);
+            } else if (variableScope != null) {
+                // otherwise, store the value in the variable scope
+                variableScope.storeInScope(variable, val);
+            } else {
+                // if the variable is not found in the function scope, create the variable in
+                // the global scope
+                ScopeRec existingScope = findScopeinStack(variable);
+                if (existingScope != null) {
+                    existingScope.storeInScope(variable, val);
+                } else {
+                    declareVariable(variable);
+                    currentScope.storeInScope(variable, val);
+                }
+            }
         }
     }
 
